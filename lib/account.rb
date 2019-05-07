864ac00e5531b4789edf7fbe007a31bc2411bb12
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-05-07 16:53:28 +0100

    ADD: Can deposit money into account
    Added a unit test and code for a deposit method on Account class which takes a number as argument and adds it to balance

--- lib/account.rb
+++ lib/account.rb
@@ -6,4 +6,8 @@ class Account
     @balance = 0
     @history = []
   end
+
+  def deposit(value)
+    @balance += value
+  end
 end
--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -10,4 +10,11 @@ describe Account do
   it 'should have empty history array when initialised' do
     expect(account.history).to eq []
   end
+
+  describe '#deposit' do
+    it 'should add value to balance' do
+      account.deposit(50)
+      expect(account.balance).to eq 50
+    end
+  end
 end
