73559bd12c41e3077b963492e2d9db3c2aa56683
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-05-08 13:53:28 +0100

    REFACTOR: Changed acceptance criteria test to use account statement method

--- spec/features/acceptance_criteria_spec.rb
+++ spec/features/acceptance_criteria_spec.rb
@@ -13,7 +13,7 @@ describe 'Acceptance Criteria for Bank app' do
     expected_result = ['date || credit || debit || balance',
                        '14/01/2012 || || 500.00 || 2500.00',
                        '13/01/2012 || 2000.00 || || 3000.00',
-                       '10/01/2012 || 1000.00 || || 1000.00'].join("\n")
-    expect(Printer.statement(account)).to eq expected_result
+                       "10/01/2012 || 1000.00 || || 1000.00\n"].join("\n")
+    expect { account.statement }.to output(expected_result).to_stdout
   end
 end
